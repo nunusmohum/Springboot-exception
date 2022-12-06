3f62f92aad212960050931ff4ffe880142c68030
nunusmohum <halojw301@gmail.com>
2022-12-06 15:07:44 +0900

    서블릿 예외 처리 - 오류 페이지 작동 원리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -12,7 +12,7 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
 
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/400");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/500");
 
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
